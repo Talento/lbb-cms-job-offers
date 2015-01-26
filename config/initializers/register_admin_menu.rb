231e4af432df075aaa464730fabcafa8338b6231
Alejandro León <alejandro.leon@the-cocktail.com>
2015-01-26 12:27:17 +0100

    add the menu item to resource items

--- config/initializers/register_admin_menu.rb
+++ config/initializers/register_admin_menu.rb
@@ -1,5 +1,5 @@
 if defined? WilsonCms::AdminMenus
-  WilsonCms::AdminMenus.menu_items << {
+  WilsonCms::AdminMenus.resource_items << {
     key: :job_offers,
     model_name: 'WilsonCmsJobOffers::JobOffer',
     url:  proc { WilsonCmsJobOffers::Engine.routes.url_helpers.admin_job_offers_path }
