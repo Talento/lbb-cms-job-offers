ca972daf39032e1949c1ef11c566ff9fcd15fcf9
Alejandro León <alejandro.leon@the-cocktail.com>
2015-07-29 12:27:33 +0200

    added more fields to job_offer_applications

--- db/migrate/20141016083954_create_wilson_cms_job_offers_job_applications.rb
+++ db/migrate/20141016083954_create_wilson_cms_job_offers_job_applications.rb
@@ -3,6 +3,10 @@ class CreateWilsonCmsJobOffersJobApplications < ActiveRecord::Migration
     create_table :wilson_cms_job_offers_job_applications do |t|
       t.references :job_offer, index: true
       t.attachment :cv
+      t.string :name
+      t.string :phone_number
+      t.string :linkedin_profile
+      t.string :email
       t.timestamps
     end
   end
