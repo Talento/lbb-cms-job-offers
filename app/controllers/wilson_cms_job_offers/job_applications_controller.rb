55a7885b150598421efebaa347a966c53b2e2260
Alejandro León <alejandro.leon@the-cocktail.com>
2015-07-30 15:57:33 +0200

    allow to create job application without offer

--- app/controllers/wilson_cms_job_offers/job_applications_controller.rb
+++ app/controllers/wilson_cms_job_offers/job_applications_controller.rb
@@ -1,21 +1,32 @@
 module WilsonCmsJobOffers
   class JobApplicationsController < EngineController
 
-    inherit_resources
+
 
     def new
-      @job_offer = JobOffer.friendly.find params[:job_offer_id]
+      @job_offer = JobOffer.friendly.find(params[:job_offer_id]) if params[:job_offer_id]
       new! { job_offers_path }
     end
 
     def create
-      create! { job_offers_path  }
+      @job_offer = JobOffer.find( params[:job_application][:job_offer_id] ) if params[:job_application][:job_offer_id]
+      @job_application = JobApplication.new( permitted_params[:job_application] )
+
+      if @job_application.valid?
+        @job_application.save
+        redirect_to job_offers_path
+        flash[:notice] = I18n.t('job_applications.create.success')
+      else
+        redirect_url = @job_offer.present?? job_offer_path(@job_offer) : job_offers_path
+        flash[:error] = I18n.t('job_applications.create.error')
+        redirect_to redirect_url
+      end
     end
 
     private
 
     def permitted_params
-      params.permit(job_application: [:job_offer_id, :cv])
+      params.permit(job_application: [:job_offer_id, :cv, :name, :phone_number, :linkedin_profile, :email])
     end
 
   end
--- app/models/wilson_cms_job_offers/job_application.rb
+++ app/models/wilson_cms_job_offers/job_application.rb
@@ -3,7 +3,6 @@ module WilsonCmsJobOffers
     has_attached_file :cv
     belongs_to :job_offer
 
-    validates :job_offer, presence: true
     validates_attachment :cv, content_type: { content_type: ["application/pdf", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"] }
   end
 end
