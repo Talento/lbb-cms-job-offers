143912a462792a7c19f91ad3208456d7fcfc952e
Alejandro León <alejandro.leon@the-cocktail.com>
2015-07-30 15:58:08 +0200

    refactors to admin pages

--- config/initializers/register_admin_menu.rb
+++ config/initializers/register_admin_menu.rb
@@ -1,7 +1,9 @@
-if defined? WilsonCms::AdminMenus
-  WilsonCms::AdminMenus.resource_items << {
-    key: :job_offers,
-    model_name: 'WilsonCmsJobOffers::JobOffer',
-    url:  proc { WilsonCmsJobOffers::Engine.routes.url_helpers.admin_job_offers_path }
-  }
+Rails.application.config.after_initialize do
+  if defined? WilsonCms::AdminMenus
+    WilsonCms::AdminMenus.resource_items << {
+      key: :job_offers,
+      model_name: 'WilsonCmsJobOffers::JobOffer',
+      url:  proc { WilsonCmsJobOffers::Engine.routes.url_helpers.admin_job_offers_path }
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ WilsonCmsJobOffers::Engine.routes.draw do
   namespace :admin do
     resources :job_offers
   end
-  
+
   localized do
-    resources :job_offers, only: [:index, :show] do
-      resources :job_applications, only: [:new, :create]
-    end
+    resources :job_offers, only: [:index, :show]     
+    resources :job_applications, only: [:create]
   end
 
 end
