259a3c3d171843b3d59faf4f273284690e9bfd3d
Alejandro León <alejandro.leon@the-cocktail.com>
2015-08-11 12:44:37 +0200

    added presence validation

--- app/models/wilson_cms_job_offers/job_application.rb
+++ app/models/wilson_cms_job_offers/job_application.rb
@@ -5,6 +5,8 @@ module WilsonCmsJobOffers
 
     validates_attachment :cv, content_type: { content_type: ["application/pdf", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"] }
 
+    validates :cv, :name, presence: true
+
     scope :with_name, ->(name){where("name like '%#{name}%' or email like '%#{name}%'")}
 
   end
